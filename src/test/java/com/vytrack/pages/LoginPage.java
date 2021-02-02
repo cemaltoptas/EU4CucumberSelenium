c6c979292e1e9fbbf4955b2422d7e6a97ae45d7e
cemal <cemaltoptas55@gmail.com>
2021-02-02 20:57:31 +0100

    updated

--- src/test/java/com/vytrack/pages/LoginPage.java
+++ src/test/java/com/vytrack/pages/LoginPage.java
@@ -1,7 +1,7 @@
 package com.vytrack.pages;
 
-import com.cybertek.utilities.ConfigurationReader;
-import com.cybertek.utilities.Driver;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
--- src/test/java/com/vytrack/step_definitions/LoginStepsDefs.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepsDefs.java
@@ -1,24 +1,46 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.DashBoardPage;
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
 
 public class LoginStepsDefs {
 
     @Given("The user is on the login page")
     public void the_user_is_on_the_login_page() {
-        System.out.println("I open the web browser");
+        String url = ConfigurationReader.get("url");
+       // WebDriver driver = Driver.get();
+       // driver.get(url);   if we create the object url we will not allowed to use
+        // driver object in other blocks
+        //Using Singleton is the best approach
+        Driver.get().get(url);
     }
     @When("The user enters driver information")
     public void the_user_enters_credentials() {
-            System.out.println("Enter valid username and password");
+
+        String userName = ConfigurationReader.get("driver_username");
+        String password = ConfigurationReader.get("driver_password");
+
+        LoginPage loginPage = new LoginPage();
+       // loginPage.username.sendKeys(userName);
+       // loginPage.password.sendKeys(password);
+       // loginPage.submitBtn.click();
+        loginPage.login(userName,password);
     }
 
     @Then("The user should be able to login")
     public void the_user_should_be_able_to_login() {
-        System.out.println("You are logged in");
+
+        String actualTitle = Driver.get().getTitle();
+        // When using Assert.assertEquals in junit , first expected then actual!!!
+        Assert.assertEquals("Dashboard",actualTitle);
     }
     @When("The user enters sales manager credentials")
     public void the_user_enters_sales_manager_credentials() {
